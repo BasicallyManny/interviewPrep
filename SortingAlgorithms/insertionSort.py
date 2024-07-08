abd52f7c10b8751f50916bcb41092186320e3439
Manny Fong <mafong081102@gmail.com>
2024-07-08 14:57:10 -0400

    fixed an oopsies

--- SortingAlgorithms/insertionSort.py
+++ SortingAlgorithms/insertionSort.py
@@ -7,27 +7,28 @@ def insertionSort(array):
     Args:
         array (list): The array to be sorted.
     """
-    
+
     for j in range(1,len(array)):
         # Print the unsorted array
         print("Unsorted array: ",array)
-        
+
         # The key to be inserted
         key=array[j]
         print("key: ",key)
-        
+
         i=j-1
-        
+
         # Shift elements of the array, greater than the key, to the right
         while i>=0 and array[i]>key:
             array[i+1]=array[i]
             i=i-1
-            
+
         # Place the key at its correct position
         array[i+1]=key  
-        
+
         # Print the sorted array
-        return array
-        print("Sorted array: ",array)  
+        print("Sorted array: ",array) 
         
-print("Final sorted array: ",insertionSort(array))
+    return array 
+
+print("final sorted array: ", insertionSort(array))
